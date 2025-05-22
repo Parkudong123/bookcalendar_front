589e086df06445528d8f2dc8493d11fc3313405f
parkudong <[1224wh@gachon.ac.kr]>
2025-05-23 08:16:49 +0900

    1.07 version

--- app/login.tsx
+++ app/login.tsx
@@ -32,7 +32,7 @@ export default function LoginScreen() {
       console.log('✅ 로그인 성공!', response.data);
       router.replace('/main');
     } catch (error) {
-      console.error('❌ 로그인 실패:', error);
+      
       alert('로그인에 실패했습니다. 다시 확인해주세요.');
     }
   };
